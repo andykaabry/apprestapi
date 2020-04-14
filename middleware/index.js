7742f7149fda4f2c33499cf249dbf8fda4b4f5e5
andyka.abry <andyka.abry@outlook.com>
2020-04-14 18:17:22 +0700

    login user

--- middleware/index.js
+++ middleware/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var auth = require('./auth');
 var router = express.Router();
 
-
+//daftarkan menu registrasi
 router.post('/api/v1/register', auth.registrasi);
+router.post('/api/v1/login', auth.login);
 
 
 module.exports = router;
